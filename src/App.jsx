815f6e306fd803e8b0ceded0fb71d58d3bada6d3
Benny Yang <bennyyang179@gmail.com>
2023-09-06 21:29:05 +1000

    added assets and fonts

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+import neon from './assets/neon2.webp';
+
+const App =()=>{
+
+    return(
+        <div>
+            <h1>Wassup</h1>
+            <img src={neon}/>
+        </div>
+        
+    )
+    
+}
+
+export default App;
