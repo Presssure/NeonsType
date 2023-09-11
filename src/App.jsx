d63fcf5b71ea6b66a2e566e1075eb744dc4531fc
Benny Yang <bennyyang179@gmail.com>
2023-09-11 21:32:16 +1000

    worked on checking and backspae logic

--- src/App.jsx
+++ src/App.jsx
@@ -52,11 +52,69 @@ class App extends React.Component {
   // whereas a normal function's scope is defined on run time and will have the scope of the instance that called it(TypingChallenge??)
 
   handleUserInput = (input) => {
-    console.log(input);
-    console.log(this.state.timerStarted);
+
+    /**
+         * 1. Handle the underflow case - all characters should be shown as not-attempted
+         * 2. Handle the overflow case - early exit
+         * 3. Handle the backspace case
+         *      - Mark the [index+1] element as notAttempted
+         *        (irrespective of whether the index is less than zero)
+         *      - But, don't forget to check for the overflow here
+         *        (index + 1 -> out of bound, when index === length-1)
+         * 4. Update the status in test info
+         *      1. Find out the last character in the inputValue and it's index
+         *      2. Check if the character at same index in testInfo (state) matches
+         *      3. Yes -> Correct
+         *         No  -> Incorrect (Mistake++)
+         * 5. Irrespective of the case, characters, words and wpm can be updated
+         */
+
+    const characters = input.length;
+    const words = input.split(" ").length;
+    const index = characters - 1;
+
+    // TODO handle an edge case where yuo highlight the word and delete the entire word or words
+
+    // underflow only handling back spaces as of now
+    if(index<0){
+      this.setState({testInfo:[{testLetter:this.state.testInfo[0].testLetter, status:"notAttempted"},
+        ...this.state.testInfo.splice(1)],characters,words})
+
+        return;
+    }
+    
+    // overflow
+    if(index>= this.state.selectedParagraph.length){
+      this.setState({characters, words});
+      return;
+    }
+
+    // Make a copy of testInfo
+
+    const testInfo =this.state.testInfo;
+    if(!index === this.state.selectedParagraph.length-1){
+      testInfo[index+1].status="notAttempted";
+    }
+
+    // check for the correct type letters
+    const isCorrect=input[index]===testInfo[index].testLetter;
+
+    // update testInfo
+    testInfo[index].status=isCorrect ? "correct":"incorrect"
+
+    // update the state
+    this.setState({
+      testInfo,
+      words,
+      characters
+    })
+
+
     if (!this.state.timerStarted) {
       this.startTimer();
     }
+
+
   };
 
   startTimer = () => {
